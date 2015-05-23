c94b0006d9c22d5d8a26a773058271efb589eb07
Admin <Admin@D20908>
2015-05-23 15:57:39 +0700

    aa

---
+++ example02/src/example02/Rectangle.java
@@ -0,0 +1,14 @@
+package example02;
+
+public class Rectangle {
+	int width;
+	int height;
+	public Rectangle(int a,int  b){
+		width=a;
+		height=b;
+		
+	}
+public int area(){
+	return (width*height);
+}
+}
